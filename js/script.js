7afe7be47b05c2b7e5b40dff344318b09bd98d9e
Bykov Denis <stivenrain@gmail.com>
2016-03-31 08:44:58 +0300

    Работа над API картами и добавлена картинка над картой в мобильной версии

--- js/script.js
+++ js/script.js
@@ -33,7 +33,31 @@ var main_icon = document.getElementById("main_icon");
     //main_icon.classList.toggle("main-nav__icon");
 
 
-  })
+  });
+
+
+// Показать карту
+
+var contacts_map = document.querySelector(".contacts__map");
+
+
+
+    var piter = {lat: 59.9387942, lng: 30.323083300000008};
+    var center = {lat:59.939462, lng:30.316732};   //59.939462, 30.316732
+    var map = new google.maps.Map(document.getElementById('map'), {
+      scaleControl: true,
+      center: center,
+      zoom: 16
+    });
+
+    var infowindow = new google.maps.InfoWindow;
+    infowindow.setContent('<b>г. Санкт-Петербург, ул. Б. Конюшенная, д. 19</b>');
+
+    var marker = new google.maps.Marker({map: map, position: piter});
+    marker.addListener('click', function() {
+      infowindow.open(map, marker);
+    });
+
 
 
 
