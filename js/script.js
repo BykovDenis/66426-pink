b642009a301cb6df72bf1508edbb9fdd8452833c
Bykov Denis <stivenrain@gmail.com>
2016-04-01 08:38:06 +0300

    Нарисована карта на js с ресайзом

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 /**
  * Created by Denis on 31.03.2016.
  */
-
+/*
 var main_nav = document.getElementById("main_nav");
 var main_link = document.getElementById("main_link");
 var main = document.querySelector(".main-nav");
@@ -35,28 +35,87 @@ var main_icon = document.getElementById("main_icon");
 
   });
 
-
+*/
 // Показать карту
-
+/*
 var contacts_map = document.querySelector(".contacts__map");
 
-
-
-    var piter = {lat: 59.9387942, lng: 30.323083300000008};
-    var center = {lat:59.939462, lng:30.316732};   //59.939462, 30.316732
+    var piter = {lat: 59.936421, lng: 30.321119};
+    var center = {lat: 59.936077, lng: 30.321108};
     var map = new google.maps.Map(document.getElementById('map'), {
       scaleControl: true,
       center: center,
       zoom: 16
     });
 
-    var infowindow = new google.maps.InfoWindow;
-    infowindow.setContent('<b>г. Санкт-Петербург, ул. Б. Конюшенная, д. 19</b>');
-
-    var marker = new google.maps.Marker({map: map, position: piter});
+    image = "img/icon-map-marker.svg";
+    var marker = new google.maps.Marker({map: map, position: piter, icon: image});
     marker.addListener('click', function() {
       infowindow.open(map, marker);
     });
+*/
+/*
+
+function initialize() {
+
+  var contacts_map = document.querySelector(".contacts__map");
+
+  var piter = {lat: 59.936421, lng: 30.321119};
+  var center = {lat: 59.936077, lng: 30.321108};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    scaleControl: true,
+    center: center,
+    zoom: 17
+  });
+
+  image = "img/icon-map-marker.svg";
+  var marker = new google.maps.Marker({map: map, position: piter, icon: image});
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
+
+
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+*/
+
+function initMap() {
+
+    var map;
+
+    var marker = {lat: 59.936421, lng: 30.321119};
+    var center = {lat: 59.937078, lng: 30.320990};
+
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: center,
+      zoom: 16,
+      disableDefaultUI: true,
+      mapTypeControlOptions: {
+        style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
+      }
+    });
+
+    image = "img/icon-map-marker.svg";
+    var marker = new google.maps.Marker({map: map, position: marker, icon: image});
+    marker.addListener('click', function () {
+      infowindow.open(map, marker);
+    });
+
+  }
+
+// Перерисовка при смене размера страницы
+window.addEventListener('resize', function(){
+
+  if(google)
+    initMap();
+
+});
+
+
+
+
+
 
 
 
